394283b2aeda3f89a377f751d9c5e788249627b6
potatocat7 <potatocat7@gmail.com>
2021-12-04 16:33:40 +0900

    C#でのRESTサーバークライアント側確認用

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+
+namespace CS_study_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comment = "あいうえお";
+            //var content = new ByteArrayContent(Encoding.UTF8.GetBytes(comment));
+
+            var content = new StringContent(comment, Encoding.UTF8);
+ 
+            HttpClient client = new HttpClient();
+            while ( true )
+            {
+                //var res = client.GetAsync("http://localhost:8080/").Result;
+                var res = client.PostAsync("http://localhost:8080/a",content).Result;
+                var text = res.Content.ReadAsStringAsync().Result;
+                if(text != null){
+                    Console.WriteLine($"{text}");
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
